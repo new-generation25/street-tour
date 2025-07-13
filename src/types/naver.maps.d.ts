0325fced641d174d104e76d3c4623805ba155bb7
이권 <socialceos@gmail.com>
2025-07-14 01:13:12 +0900

    fix: Vercel 빌드 오류 수정 (no-explicit-any)

--- src/types/naver.maps.d.ts
+++ src/types/naver.maps.d.ts
@@ -2,6 +2,7 @@
 interface NaverMap {
   // 사용할 네이버 지도 관련 타입들을 여기에 정의할 수 있습니다.
   // 우선은 간단하게 any로 처리하여 오류를 해결합니다.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any;
 }
 
