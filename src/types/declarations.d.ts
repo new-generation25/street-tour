641aed6294bd65a01a4e0a545f219eb13e3d34f9
이권 <socialceos@gmail.com>
2025-07-14 04:11:07 +0900

    fix: QrScanner 타입 선언 상세화

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -1,1 +1,15 @@
-declare module 'react-qr-scanner'; 
+declare module 'react-qr-scanner' {
+  import * as React from 'react';
+
+  interface QrScannerProps {
+    delay?: number | boolean;
+    onError: (err: unknown) => void;
+    onScan: (data: { text: string } | null) => void;
+    style?: React.CSSProperties;
+    facingMode?: 'user' | 'environment';
+  }
+
+  const QrScanner: React.FC<QrScannerProps>;
+
+  export default QrScanner;
+} 
