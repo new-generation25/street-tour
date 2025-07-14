e3bd0e2f8ef692ea3a060a6d3aa1d2f361cb2676
이권 <socialceos@gmail.com>
2025-07-14 21:37:15 +0900

    feat: update to purple/lavender/blue/orange/red color scheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#5F4073" />
+        <meta name="theme-color" content="#4F238C" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
